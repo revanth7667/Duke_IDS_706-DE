f3cba5d6dc0df08bc858cf4a4c38dd346f573888
Revanth Chowdary <46459100+revanth7667@users.noreply.github.com>
2023-09-08 15:06:27 -0400

    Update test_example.py

--- test_example.py
+++ test_example.py
@@ -1,6 +1,9 @@
 from example import custom_add
 
 def test_example():
-    # print(longestSubstring("abcdcca"))
     assert custom_add(2,3) == 5
+    assert custom_add(4,5) == 9
+    assert custom_add(-1,1) == 0
+    assert custom_add(-1,-3) == -4
+    assert custom_add(0,0) == 0
     pass
